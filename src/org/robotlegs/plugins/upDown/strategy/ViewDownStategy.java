10f912b26535372c32f41aea8e1e43d2a0d988c6
squeedee <squeedee@gmail.com>
2010-04-01 18:48:25 +1300

    Some debug to work out if im doing references right. Happy easter all.

--- src/org/robotlegs/plugins/upDown/strategy/ViewDownStategy.java
+++ src/org/robotlegs/plugins/upDown/strategy/ViewDownStategy.java
@@ -1,7 +1,16 @@
 package org.robotlegs.plugins.upDown.strategy;
 
+import com.intellij.lang.javascript.psi.ecmal4.JSClass;
+import com.intellij.lang.javascript.psi.resolve.JSResolveUtil;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.DataKeys;
 import com.intellij.openapi.actionSystem.Presentation;
+import com.intellij.psi.PsiReference;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.search.searches.ReferencesSearch;
+import org.robotlegs.plugins.util.JSFileUtil;
+
+import java.util.Collection;
 
 /**
  * Created by IntelliJ IDEA. User: rasheed Date: 31/03/2010 Time: 8:15:19 AM To change this template use File | Settings
@@ -17,12 +26,23 @@ public class ViewDownStategy implements IUpDownStrategy {
 	}
 
 	public void perform(AnActionEvent event) {
-//		JSClass clazz = JSFileUtil.getJSClass(event);
+		JSClass clazz = JSFileUtil.getJSClass(event);
 
-//		if (clazz == null)
-//			return;
+		if (clazz == null)
+			return;
 
-		// Todo hunt for a the classes references (clazz in the references catalog)
+		// Todo hunt for the classes' references (clazz in the references catalog)
+		GlobalSearchScope scope = JSResolveUtil.getSearchScopeWithPredefined(event.getData(DataKeys.PROJECT));
+
+		Collection<PsiReference> references = ReferencesSearch.search(clazz,scope,false).findAll();
+
+		for (PsiReference reference : references) {
+			System.out.println("***-1-*** " + reference.getRangeInElement().toString());
+			System.out.println("***-2-*** " + reference.getElement().getContainingFile().getName());
+			System.out.println("***-3-*** " + reference.toString());
+		}
+
+		//Collection<PsiReference> ReferencesSearc ReferencesSearc.search(clazz, scope, false).findAll();
 
 		//ActionEventUtil.getIdeView(event).selectElement(viewElement);
 	}
